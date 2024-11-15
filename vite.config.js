94fb9f1fbe6177aca155dd26d6c8d9b81e3169b1
DeepanshuRathore23 <deepanshurathore2004@gmail.com>
2024-11-15 17:34:25 +0530

    updated vite.config,js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Erudite_School/', // for GitHub Pages
+  base: '/', // for GitHub Pages
 })
